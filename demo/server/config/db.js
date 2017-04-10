14e8b150c9419727a6b1e46921486e00a5f37bc3
Copyes <fan_545697167@qq.com>
2017-04-10 18:36:52 +0800

    db

--- demo/server/config/db.js
+++ demo/server/config/db.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
-
+// 使用url连接的形式进行连接，注意将root: 后面的XXXX改成自己数据库的密码
 const TodoList = new Sequelize('mysql://root:123456@localhost/todolist',{
     define: {
-        timestemps: false
+        timestamps: false  // 取消Sequelzie自动给数据表加入时间戳（createdAt以及updatedAt）
     }
 });
 
